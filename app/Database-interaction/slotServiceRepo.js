b1df92717b436fc30bf6a5ad8863a4772609ae0c
shivamsouravjha <shivam.jha@smallcase.com>
2023-06-26 16:38:25 +0530

    booking ticket

--- app/Database-interaction/slotServiceRepo.js
+++ app/Database-interaction/slotServiceRepo.js
@@ -1,12 +1,10 @@
 import SlotSchema from "../Model/slot";
-
-function arrayDifference(array1, array2,numberOfSeats) {
-    var differentArray  = false
+import TicketSchema from "../Model/ticket"
+function arrayDifference(array1, array2, numberOfSeats) {
+    var differentArray = false
     array2.forEach((element) => {
-        console.log(array1.includes(element))
-        if (array1.includes(element) || element>numberOfSeats) {
-            console.log("Asd")
-            differentArray =  true;
+        if (array1.includes(element) || element > numberOfSeats) {
+            differentArray = true;
         }
     });
     return differentArray
@@ -48,46 +46,30 @@ export default class SourceRepository {
     async bookTickets(obj) {
         const { movieslot, seats, movie, theatre } = obj
         try {
-            const slots = await SlotSchema.findOne({ movie: movie, theatre: theatre })
-            let bookedSeats = arrayDifference(slots.occupiedSeats[movieslot], seats,slots.numberOfSeats)
-            console.log(bookedSeats,slots.occupiedSeats[movieslot], seats)
+            const slots = await SlotSchema.findOne({ movie: movie, theatre: theatre }).populate('movie').populate('theatre')
+            let bookedSeats = arrayDifference(slots.occupiedSeats[movieslot], seats, slots.numberOfSeats)
             if (bookedSeats) {
-                throw new Error('Seat already booked.');   
+                throw new Error('Seat already booked.');
             }
             slots.occupiedSeats[movieslot].push(...seats)
-            let f = await slots.save()
-            // let theatre, show = [];
-            // for (let i = 0; i < slots.length; i++) {
-            //     let eligibleShows = []
-            //     theatre = await TheatreSchema.find({ _id: slots[i].theatre })
-
-            //     if (slots[i].slots.includes(slot)) {
-            //         for (let j = 0; j < theatre.length; j++) {
-            //             console.log(theatre, "DSdsds")
-            //             console.log(theatre[j].occupiedSeats[slots[i].slots.indexOf(slot)].length)
-            //             console.log(theatre[j].numberOfSeats, "sf")
-            //             if (theatre[j].occupiedSeats[slots[i].slots.indexOf(slot)].length >= theatre[j].numberOfSeats) {
-            //                 console.log(theatre[i])
-            //             } else {
-            //                 console.log(theatre, "DSdsds")
-
-            //                 eligibleShows.push({ slot: slots[i].slots[slots[i].slots.indexOf(slot)], theatre: theatre[i]._id, })
-            //             }
-            //         }
-            //     } else {
-            //         console.log(theatre, "DSdsds")
-            //         for (let j = 0; j < theatre.length; j++) {
-            //             theatre[j].occupiedSeats.forEach((element) => {
-            //                 if (element.length < theatre[j].numberOfSeats) {
-            //                     eligibleShows.push({ slot: slots[i].slots[i], theatre: theatre[i]._id, })
-            //                 }
-            //             });
-            //         }
-            //     }
-
-            //     show.push(...eligibleShows)
-            // }
-            return { 'message': 'Group Joined', 'slots': f };
+            await slots.save()
+            let ticket = new TicketSchema({
+                seatNumber: seats,
+                theatre: theatre,
+                movie: movie,
+                slot: slots.slots[movieslot],
+                active: true,
+            })
+            await ticket.save();
+            return {
+                'message': 'Group Joined', 'ticket': {
+                    seatNumber: seats,
+                    theatre: slots.theatre.name,
+                    movie: slots.movie.name,
+                    slot: slots.slots[movieslot],
+                    active: true,
+                }
+            };
         } catch (error) {
             throw error
         }
--- app/Model/ticket.js
+++ app/Model/ticket.js
@@ -3,9 +3,10 @@ import mongooseUniqueValidator from 'mongoose-unique-validator';
 const schema = mongoose.Schema;
 
 const TicketSchema = new schema({
-    seatNumber: { type: String },
-    name: { type: String },
-    slot: { type: Boolean },
+    seatNumber: [{ type: String }],
+    theatre: {type :mongoose.Types.ObjectId,required:true,ref:'Theatre'},
+    movie: {type :mongoose.Types.ObjectId,required:true,ref:'Show'},
+    slot: { type: String },
     active: {type : Boolean},
 }, {
     versionKey: false
