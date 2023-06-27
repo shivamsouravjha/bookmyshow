899a4c95d83a3a1be7024b66a4efa216809f4034
shivamsouravjha <shivam.jha@smallcase.com>
2023-06-27 15:16:59 +0530

    moving slot from repo to service level

--- app/Database-interaction/slotServiceRepo.js
+++ app/Database-interaction/slotServiceRepo.js
@@ -12,62 +12,26 @@ function arrayDifference(array1, array2, numberOfSeats) {
 }
 
 export default class SourceRepository {
-    async findSlot(obj) {
-        const { slot, movie } = obj
-        try {
-            let movieData = await ShowSchema.find({
-                name: {
-                    $type: 'string',
-                    $regex: new RegExp(movie, 'i'),
-                },
-            })
-            let show = [];
-
-            for (let mov = 0; mov < movieData.length; mov++) {
-                const slots = await SlotSchema.find({
-                    movie: movieData[mov]._id
-                }).populate('theatre');
-                for (let i = 0; i < slots.length; i++) {
-                    let eligibleShows = []
-                    if (slots[i].slots.includes(slot)) {
-                        if (slots[i].occupiedSeats[slots[i].slots.indexOf(slot)].length >= slots[i].numberOfSeats) {
-                            slots[i].occupiedSeats.forEach((element, item) => {
-                                if (element.length < slots[i].numberOfSeats) {
-                                    eligibleShows.push({ slotText: slots[i].slots[item], slot: item, theatre: slots[i].theatre._id, theatreName: slots[i].theatre.name, movieId: movieData[mov]._id })
-                                }
-                            });
-                        } else {
-                            eligibleShows.push({ slotText: slots[i].slots[slots[i].slots.indexOf(slot)], slot: slots[i].slots.indexOf(slot), theatre: slots[i].theatre._id, theatreName: slots[i].theatre.name, movieId: movieData[mov]._id })
-                        }
-                    } else {
-                        slots[i].occupiedSeats.forEach((element, item) => {
-                            if (element.length < slots[i].numberOfSeats) {
-                                eligibleShows.push({ slotText: slots[i].slots[item], slot: item, theatre: slots[i].theatre._id, theatreName: slots[i].theatre.name, movieId: movieData[mov]._id })
-                            }
-                        });
-
-                    }
-                    show.push({ name: movieData[mov].name, shows: eligibleShows })
-                }
-            }
-            if (show.length == 0) {
-                throw new Error("No slots available, try searching for anohter movie maybe?")
-            }
-            return { 'message': 'Slots are available', 'slots': show };
-        } catch (error) {
-            throw error
-        }
+    async findMovie(movie) {
+        let movieData = await ShowSchema.find({
+            name: {
+                $type: 'string',
+                $regex: new RegExp(movie, 'i'),
+            },
+        })
+        return movieData
     }
+
+    async findSlotByTheatre(movieId) {
+        const slots = await SlotSchema.find({
+            movie: movieId,
+        }).populate('theatre');
+        return slots
+    }
+
     async bookTickets(obj) {
-        const { movieslot, seats, movie, theatre } = obj
+        const { movieslot, seats, movie, theatre, slots } = obj
         try {
-            const slots = await SlotSchema.findOne({ movie: movie, theatre: theatre }).populate('movie').populate('theatre')
-            let bookedSeats = arrayDifference(slots.occupiedSeats[movieslot], seats, slots.numberOfSeats)
-            if (bookedSeats) {
-                throw new Error('Seat already booked.');
-            }
-            slots.occupiedSeats[movieslot].push(...seats)
-            await slots.updateOne()
             let ticket = new TicketSchema({
                 seatNumber: seats,
                 theatre: theatre,
@@ -76,34 +40,21 @@ export default class SourceRepository {
                 active: true,
             })
             await ticket.save();
-            return {
-                'message': 'Tickets booked successfully', 'ticket': {
-                    seatNumber: seats,
-                    theatre: slots.theatre.name,
-                    movie: slots.movie.name,
-                    slot: slots.slots[movieslot],
-                    active: true,
-                    id: ticket._id,
-                }
-            };
+            return ticket;
         } catch (error) {
             throw error
         }
     }
 
+    async udpateSlot(slots) {
+        return await slots.updateOne();
+    }
+
     async showAvailableTickets(obj) {
-        const { movieslot, movie, theatre } = obj
+        const { movie, theatre } = obj
         try {
             const slot = await SlotSchema.findOne({ movie: movie, theatre: theatre })
-            let seats = [];
-            for (var i = 0; i < slot.numberOfSeats; i++) {
-                if (!slot.occupiedSeats[movieslot].includes(i + 1))
-                    seats.push(i + 1);
-            }
-            if (seats.length < 1) {
-                throw new Error("No availavble tickets")
-            }
-            return { 'message': 'Available tickets', 'seats': seats };
+            return slot
         } catch (error) {
             throw error
         }
@@ -126,7 +77,7 @@ export default class SourceRepository {
         try {
             const sess = await mongoose.startSession();
             sess.startTransaction();
-            await slot.updateOne();
+            await this.udpateSlot(slot);
             await ticket.updateOne();
             await sess.commitTransaction();
             return { 'message': 'Ticket Cancelled Successfully', 'seats': ticket };
@@ -143,5 +94,9 @@ export default class SourceRepository {
         } catch (error) {
             throw error
         }
+    }
+
+    async searchSlot(obj) {
+        return SlotSchema.findOne(obj).populate('movie').populate('theatre')
     }
 }
--- app/Service/bookingSlotService.js
+++ app/Service/bookingSlotService.js
@@ -8,8 +8,37 @@ export default class BookingService {
     //showing slots
     async showSlot(args) {
         try {
-            let accountInfo = await this.repository.findSlot(args);
-            return accountInfo
+            const { slot, movie } = obj
+            let movieData = await this.repository.findMovie(movie);
+            for (let mov = 0; mov < movieData.length; mov++) {
+                const slots = await this.repository.findSlotByTheatre(movieData[mov]._id)
+                for (let i = 0; i < slots.length; i++) {
+                    let eligibleShows = []
+                    if (slots[i].slots.includes(slot)) {
+                        if (slots[i].occupiedSeats[slots[i].slots.indexOf(slot)].length >= slots[i].numberOfSeats) {
+                            slots[i].occupiedSeats.forEach((element, item) => {
+                                if (element.length < slots[i].numberOfSeats) {
+                                    eligibleShows.push({ slotText: slots[i].slots[item], slot: item, theatre: slots[i].theatre._id, theatreName: slots[i].theatre.name, movieId: movieData[mov]._id })
+                                }
+                            });
+                        } else {
+                            eligibleShows.push({ slotText: slots[i].slots[slots[i].slots.indexOf(slot)], slot: slots[i].slots.indexOf(slot), theatre: slots[i].theatre._id, theatreName: slots[i].theatre.name, movieId: movieData[mov]._id })
+                        }
+                    } else {
+                        slots[i].occupiedSeats.forEach((element, item) => {
+                            if (element.length < slots[i].numberOfSeats) {
+                                eligibleShows.push({ slotText: slots[i].slots[item], slot: item, theatre: slots[i].theatre._id, theatreName: slots[i].theatre.name, movieId: movieData[mov]._id })
+                            }
+                        });
+
+                    }
+                    show.push({ name: movieData[mov].name, shows: eligibleShows })
+                }
+            }
+            if (show.length == 0) {
+                throw new Error("No slots available, try searching for anohter movie maybe?")
+            }
+            return { 'message': 'Slots are available', 'slots': show };
         } catch (error) {
             throw error;
         }
@@ -17,8 +46,18 @@ export default class BookingService {
     //showing tickets
     async showAvailableTickets(args) {
         try {
-            let accountInfo = await this.repository.showAvailableTickets(args);
-            return accountInfo
+            const { movieslot, movie, theatre } = args
+
+            let slotInfo = await this.repository.showAvailableTickets({ movie, theatre });
+            let seats = [];
+            for (var i = 0; i < slotInfo.numberOfSeats; i++) {
+                if (!slotInfo.occupiedSeats[movieslot].includes(i + 1))
+                    seats.push(i + 1);
+            }
+            if (seats.length < 1) {
+                throw new Error("No availavble tickets")
+            }
+            return { 'message': 'Available tickets', 'seats': seats };
         } catch (error) {
             throw error;
         }
@@ -27,8 +66,25 @@ export default class BookingService {
     //booking slots
     async bookTickets(args) {
         try {
-            let accountInfo = await this.repository.bookTickets(args);
-            return accountInfo
+            const { movieslot, seats, movie, theatre } = obj;
+            const slots = await this.repository.searchSlot({ movie, theatre })
+            let bookedSeats = arrayDifference(slots.occupiedSeats[movieslot], seats, slots.numberOfSeats)
+            if (bookedSeats) {
+                throw new Error('Seat already booked.');
+            }
+            slots.occupiedSeats[movieslot].push(...seats)
+            await this.repository.udpateSlot(slots)
+            let ticketInfo = await this.repository.bookTickets({ movieslot, seats, movie, theatre, slots });
+            return {
+                'message': 'Tickets booked successfully', 'ticket': {
+                    seatNumber: seats,
+                    theatre: slots.theatre.name,
+                    movie: slots.movie.name,
+                    slot: slots.slots[movieslot],
+                    active: true,
+                    id: ticketInfo._id,
+                }
+            };
         } catch (error) {
             throw error;
         }
@@ -38,7 +94,7 @@ export default class BookingService {
     async cancelTickets(args) {
         try {
             let ticket = await this.repository.findTicket(args);
-            if(!ticket.active){
+            if (!ticket.active) {
                 throw new Error("Already cancelled ticket")
             }
             let slot = await this.repository.findSlotForMovie(ticket);
@@ -51,7 +107,7 @@ export default class BookingService {
             }
             slot.occupiedSeats[slotNumber] = occupiedSlot;
             ticket.active = false;
-            let accountInfo = await this.repository.cancelTickets({slot,ticket});
+            let accountInfo = await this.repository.cancelTickets({ slot, ticket });
             return accountInfo
         } catch (error) {
             throw error;
